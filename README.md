3a23f118ad96471d4f7132b6c8961a81959bcebe
Jack Gumoes <j.gumoes@gmail.com>
2021-02-24 19:50:49 +0000

    root page now accessible from other devices on network

---
+++ README.md
@@ -0,0 +1,11 @@
+# Test Portal
+to run the test portal:
+```sh
+cd "ruby server test"
+rackup -o 0.0.0.0
+```
+find your computers ip address (mine is 192.168.0.229)
+
+on your phone, go to `{ip}:9292`
+
+i.e. on my phone right now, I have `192.168.0.229:9292`
--- ruby server test/libs/functions.rb
+++ ruby server test/libs/functions.rb
@@ -2,7 +2,7 @@
 def device_name_change vars
   if vars.new_name
     vars.new_name = false
-    return "This device is called #{vars.device_name}<br>"
+    return "This device is called #{vars.device_name}<br>"\
             "<b class='NameChange'>Device name changed to <em>#{vars.device_name}</em></b><br>"
   end
 end
@@ -23,15 +23,15 @@ end
 
 def scan_networks (vars)
   if vars.networks.empty?
-    return "<label class='radios'>No Networks Found"
-              "<input type='radio' name='radio' value=''>"
-              "<span class='checkmark'></span>"
+    return "<label class='radios'>No Networks Found"\
+              "<input type='radio' name='radio' value=''>"\
+              "<span class='checkmark'></span>"\
             "</label>"
   else
     vars.networks.map do |ssid|
-      "<label class='radios'>#{ssid}"
-          "<input type='radio' name='radio' value='#{ssid}'>"
-          "<span class='checkmark'></span>"
+      "<label class='radios'>#{ssid}"\
+          "<input type='radio' name='radio' value='#{ssid}'>"\
+          "<span class='checkmark'></span>"\
       "</label>"
     end.join
   end
