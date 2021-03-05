c07463dc0a87480017e6737f139bd866142d4099
Jack Gumoes <j.gumoes@gmail.com>
2021-03-05 19:55:44 +0000

    improved responsive css by a lot

--- ruby server test/libs/ServerVariables.rb
+++ ruby server test/libs/ServerVariables.rb
@@ -1,19 +1,51 @@
 class ServerVariables
-  attr_accessor :connected, :wrong_pass, :networks, :device_name, :new_name, :network_name
-  def initialize(connected = true, wrong_pass = false)
-      @connected = connected
-      @wrong_pass = wrong_pass
-      @networks = ["FBI Van number 2", "Stupid Starbucks", "Uncle Touchy's Mystery Van"]
-      @device_name = "Butt's McGee"
-      @new_name = false
+  attr_accessor :connected, :wrong_pass, :networks, :device_name, :new_name, :network_name, :last_ssid
+  def initialize(connected = false, wrong_pass = false)
+    @connected = connected
+    @wrong_pass = wrong_pass
+    @device_name = "Butt's McGee"
+    @new_name = false
+    @network_name = ""
+    @last_ssid = ""
+  end
+
+  def set_networks(empty=false)
+    if empty
+      @networks = []
+    else
+      @networks = ["FBI Van number 2", "Stupid Starbucks", "Uncle Touchy\'s Mystery Van"]
+    end
+  end
+  
+  def reset
+    # reset the flags
+    @wrong_pass = false
+    @new_name = false
+  end
+
+  def check_network(network, password)
+    # this is to simulate login attempts
+    test_network = "Stupid Starbucks"
+    test_password = "Alligator3"
+    if password == test_password
+      @network_name = network
+      @connected = true
+    else
+      p network, password
+      @wrong_pass = true
+      @connected = false
       @network_name = ""
+      @last_ssid = network
+    end
   end
 
-  def self.create(connected = true, wrong_pass = false)
+  class << self
+    def create(connected = false, wrong_pass = false)
       @vars = ServerVariables.new(connected, wrong_pass)
-  end
-
-  def self.instance
+    end
+    
+    def instance
       @vars
+    end
   end
 end
--- ruby server test/server.rb
+++ ruby server test/server.rb
@@ -5,26 +5,56 @@ require_relative 'libs/functions'
 
 class Portal < Sinatra::Base
 
-    enable :sessions
+  enable  :method_override
+  use Rack::Session::Cookie, :key => "rack.session",
+                            :path => '/',
+                            :secret => 'your_secret'
     
-#     def initialize
-#         @connected = true
-#         @wrong_pass = false
-#     end
-    before do
-        @vars = ServerVariables.instance()
+  # before (:all) do
+  #   @vars = ServerVariables.create()
+  #   session[:vars] = @vars
+  # end
+
+  get "/" do
+    @vars = session['vars']
+    # puts session.keys
+    if @vars == nil
+      @vars = ServerVariables.new()
+      session['vars'] = @vars
     end
+    # p @vars
+    # @vars = ServerVariables.instance()
+    @vars.set_networks(empty=false)
+    
+    @device_name_change = device_name_change(@vars)
 
-    get "/" do
-        @vars = ServerVariables.create()
-        @device_name_change = device_name_change(@vars)
+    @connection_status = connection_status(@vars)
+    @wrong_password = wrong_password?(@vars)
+    @wifi_ssid = scan_networks(@vars)
 
-        @connection_status = connection_status(@vars)
-        @wrong_password = test_password(@vars)
-        @wifi_ssid = scan_networks(@vars)
+    # reset flags
+    @vars.reset
 
-        erb :index
-    end
-    # # start the server if ruby file executed directly
-    run! if app_file == $0
+    erb :index
+  end
+
+  post "/wifisave" do
+    # @vars = ServerVariables.instance
+    # p @vars
+    # p session
+    @vars = session['vars']
+    puts session.keys
+    @vars.check_network(params['ssid'], params['pwd'])
+    redirect "/"
+  end
+
+  post "/change_name" do
+    @vars = session['vars']
+    @vars.device_name = params['dev_name']
+    @vars.new_name = true
+    redirect '/'
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
 end
