bfbc8bb6382f5fa2a9c96cd564c40a11f972c2a9
Jack Gumoes <j.gumoes@gmail.com>
2021-03-29 18:21:38 +0100

    altered page to build network list server-side

--- ruby server test/server.rb
+++ ruby server test/server.rb
@@ -2,13 +2,14 @@
 require 'sinatra/base'
 
 class ServerStatus
-  attr_accessor :device_name, :network_name, :wrong_pass, :name_change
+  attr_accessor :device_name, :network_name, :wrong_pass, :name_change, :attempted_network
 
   def initialize
     @device_name = "Butt's McGee"
     @network_name = false
     @wrong_pass = false
     @name_change = false
+    @attempted_network = "null"
   end
 
   def resetFlags
@@ -16,14 +17,16 @@ class ServerStatus
     @name_change = false
   end
 end
-
 class Portal < Sinatra::Base
-
+  
   enable  :method_override
   use Rack::Session::Cookie, :key => "rack.session",
-                            :path => '/',
-                            :secret => 'your_secret'
-    
+  :path => '/',
+  :secret => 'your_secret'
+  before (:all) do
+    puts ("new flag")
+    sessions['flags'] = nil
+  end
   # before (:all) do
   #   @flags = ServerVariables.create()
   #   session[:flags] = @flags
@@ -32,6 +35,7 @@ class Portal < Sinatra::Base
   get "/" do
     @flags = session['flags'] # this will actually be stored as a global variable
     if @flags == nil
+      puts ("new flag")
       # this won't be necessary because flags will be stored as a global variable
       @flags = ServerStatus.new()
       session['flags'] = @flags
@@ -40,7 +44,7 @@ class Portal < Sinatra::Base
     
     # these statements mock the String processor in ESPAsyncWebServer
     if @flags.name_change
-      @device_name_change = "<b class='NameChange'>Device name changed to <em>#{vars.device_name}</em></b><br>"
+      @device_name_change = "<b class='NameChange'>Device name changed to <em>#{@flags.device_name}</em></b><br>"
     else
       @device_name_change = ""
     end
@@ -59,8 +63,7 @@ class Portal < Sinatra::Base
 
     @device_name = @flags.device_name
 
-
-    # @wifi_ssid = scan_networks(@flags)
+    @wifi_ssid_list = build_network_list(scan_networks(), @flags.attempted_network )
 
     # reset flags
     @flags.resetFlags()
@@ -74,20 +77,32 @@ class Portal < Sinatra::Base
     # p session
     @flags = session['flags']
     puts session.keys
-    @flags.check_network(params['ssid'], params['pwd'])
+    if check_network(params['ssid'], params['pwd'])
+      @flags.network_name = params['ssid']
+    else
+      @flags.attempted_network = params['ssid']
+      @flags.network_name = false
+      @flags.wrong_pass = true
+    end
+
     redirect "/"
   end
 
   post "/change_name" do
     @flags = session['flags']
     @flags.device_name = params['dev_name']
-    @flags.new_name = true
+    @flags.name_change = true
     redirect '/'
   end
 
   get "/scan_networks" do
+    @flags = session['flags']
+    puts @flags.attempted_network
     content_type 'application/json'
-    JSON.generate({ "network_list" => scan_networks(true)})
+    JSON.generate({
+      "network_list" => scan_networks(),
+      "attempted_network" => @flags.attempted_network,
+      })
   end
 
   # start the server if ruby file executed directly
@@ -97,4 +112,37 @@ end
 def scan_networks(empty=false)
   return [] if empty
   ["FBI Van number 2", "Stupid Starbucks", "Uncle Touchy\'s Mystery Van"]
+end
+
+def check_network(name, password)
+  if name == "Uncle Touchy\'s Mystery Van" && password == "password"
+    return true
+  else
+    return false
+  end
+end
+
+def build_network_list(ssid_list, attempted_network = "")
+  if ssid_list.length == 0
+    return network_html(["No Networks Found"], "disabled")
+  end
+  html_out = ""
+  ssid_list.each do |ssid|
+    html_out += network_html(ssid, ssid == attempted_network)
+  end
+  return html_out
+end
+
+def network_html(ssid, checked=false)
+  checked_hash = {
+    true => "checked ",
+    false => "",
+    "disabled" => "disabled "}
+  
+  return "<label class='radios'>#{ssid}" +
+    "<input id=\"#{ssid}\" type='radio' name='ssid' value=\"#{ssid}\" " +
+    checked_hash[checked] +
+    "required>" +
+    "<span class='checkmark'></span>" +
+  "</label>"
 end
