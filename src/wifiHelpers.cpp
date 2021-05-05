4f2cae5115d06476e2334c560b20c2451d0094c5
Jack Gumoes <j.gumoes@gmail.com>
2021-05-05 20:24:12 +0100

    server returns result of previous connection attempt, and returns that result if the credentials are identical

--- src/wifiHelpers.cpp
+++ src/wifiHelpers.cpp
@@ -1,7 +1,16 @@
 #include <ESP8266WebServer.h>
 
-bool connectWifi(String& ssid, String& password) {
-  Serial.println("Connecting as wifi client...");
+struct lastConnectionResult {
+  String ssid = "";
+  String password = "";
+  int lastConnRes;
+} lastConn;
+
+int attemptConnection(String& ssid, String& password){
+  if (lastConn.ssid == ssid && lastConn.password == password){
+    Serial.println("credentials already attempted");
+    return lastConn.lastConnRes;
+  }
   // WiFi.disconnect();
   WiFi.begin(ssid, password);
   int connRes = WiFi.waitForConnectResult(10000);
@@ -9,6 +18,13 @@ bool connectWifi(String& ssid, String& password) {
     // this little statement is the culmination of an entire day of swearing. it is very important.
     WiFi.disconnect();
   }
+  lastConn.ssid = ssid; lastConn.password = password; lastConn.lastConnRes = connRes;
+  return connRes;
+}
+
+bool connectWifi(String& ssid, String& password) {
+  Serial.println("Connecting as wifi client...");
+  int connRes = attemptConnection(ssid, password);
   Serial.print("connRes: ");
   Serial.println(connRes);
   Serial.print("status: ");
